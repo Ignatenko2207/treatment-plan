be4de610f788c2ba45a76820900c92174c8600b1
Aleksandr Ihnatenko <oleksandr.ihnatenko@varus.ua>
2024-10-26 14:55:05 +0300

    Initial commit

---
+++ src/main/java/com/aiomed/treatmentplan/TreatmentPlanApplication.java
@@ -0,0 +1,13 @@
+package com.aiomed.treatmentplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreatmentPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreatmentPlanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aiomed/treatmentplan/TreatmentInfoPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiomed.treatmentplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreatmentInfoPlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
